30b0876903ec04692d73124c60786d3ad0281c9c
MakotoTaguchi1 <makoo.t17@gmail.com>
2025-01-07 01:36:11 +0900

    fix: remove unused security_group

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -139,28 +139,28 @@ module "vpc" {
 # }
 
 # VPCエンドポイント用のセキュリティグループ
-resource "aws_security_group" "vpc_endpoints" {
-  name        = "${var.project}-${var.environment}-vpc-endpoints"
-  description = "Security group for VPC endpoints"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "vpc_endpoints" {
+#   name        = "${var.project}-${var.environment}-vpc-endpoints"
+#   description = "Security group for VPC endpoints"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_tasks.id]
-  }
+#   ingress {
+#     from_port       = 443
+#     to_port         = 443
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.ecs_tasks.id]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-vpc-endpoints"
-    Environment = var.environment
-    Project     = var.project
-  }
-}
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-vpc-endpoints"
+#     Environment = var.environment
+#     Project     = var.project
+#   }
+# }
